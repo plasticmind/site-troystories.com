397011eb271f8835d011a35fa7b08e51ffdf7d2d
Jesse Gardner <jesse@plasticmind.com>
2022-12-15 17:19:10 -0500

    Updated site link

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,5 +1,5 @@
 module.exports = {
-  url: process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:8080' : 'http://localhost:8080',
+  url: process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:8080' : 'https://troystories.com',
   title: "Troy Stories",
   description: "Stories from the people of Troy, NY"
 };

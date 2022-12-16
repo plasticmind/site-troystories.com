f2e2dce436b62d8982757c632cf37397fa66f3f7
Jesse Gardner <jesse@plasticmind.com>
2022-12-16 15:55:29 -0500

    Added support for GA
    Included is support for tracking new pageviews when infinite scroll is triggered on the home page

--- src/assets/js/ts.js
+++ src/assets/js/ts.js
@@ -1,33 +1,30 @@
 window.addEventListener('load', function () {
     
-  // init Masonry
+  // Dummy link for storing path (for tracking analytics with infScroll)
+  let link = document.createElement('a');
+
+  // Masonry
   let msnry = new Masonry( '.story-list', {
     percentPosition: true,
     itemSelector: 'article.story',
     columnWidth: ".grid-sizer"
   });
 
-  // init Infinite Scroll
+  // Infinite Scroll
+
   let infScroll = new InfiniteScroll( '.story-list', {
     path: '.pagination__next',
     append: 'article.story',
     history: false,
     outlayer: msnry,
     hideNav: '.site-pagination'
+  }).on( 'append', function( event, response, path ) {
+    link.href = response;
+    // console.log( location + link.pathname.slice(1) );
+    ga( 'set', 'page', link.pathname.slice(1) );
+    ga( 'send', 'pageview' );
+    ga( 'send', 'event', 'Scroll Pageview', link.pathname.slice(1) );
   });
 
-  /*
-  // Add GA Pageviews
-  function listener(event, response, path) {
-    console.log('infinite scroll happened');
-    link.href = path;
-    ga( 'set', 'page', link.pathname );
-    ga( 'send', 'pageview' );
-  }
-  // link used to get absolute path, beginning with /
-  let link = document.createElement('a'); 
-
-  // add event listener
-  infScroll.on( 'append.infiniteScroll', listener);
-  */  
+  
 });

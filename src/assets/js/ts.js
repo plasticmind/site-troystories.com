424d04368843437efa29fc977c259e377c3a4ef7
Jesse Gardner <jesse@plasticmind.com>
2022-12-20 09:38:51 -0500

    Don't try infinite scroll on individual pages

--- src/assets/js/ts.js
+++ src/assets/js/ts.js
@@ -1,30 +1,36 @@
 window.addEventListener('load', function () {
+  
+  // Only activate masonry and infinite scroll if story list component exists on the page
+  var storyList = document.querySelector( '.story-list' );
+  if( typeof( storyList ) != 'undefined' && storyList != null ){
     
-  // Dummy link for storing path (for tracking analytics with infScroll)
-  let link = document.createElement('a');
+    // Dummy link for storing path (for tracking analytics with infScroll)
+    let link = document.createElement('a');
 
-  // Masonry
-  let msnry = new Masonry( '.story-list', {
-    percentPosition: true,
-    itemSelector: 'article.story',
-    columnWidth: ".grid-sizer"
-  });
+    // Masonry
+    let msnry = new Masonry( '.story-list', {
+      percentPosition: true,
+      itemSelector: 'article.story',
+      columnWidth: ".grid-sizer"
+    });
 
-  // Infinite Scroll
+    // Infinite Scroll
 
-  let infScroll = new InfiniteScroll( '.story-list', {
-    path: '.pagination__next',
-    append: 'article.story',
-    history: false,
-    outlayer: msnry,
-    hideNav: '.site-pagination'
-  }).on( 'append', function( event, response, path ) {
-    link.href = response;
-    // console.log( location + link.pathname.slice(1) );
-    ga( 'set', 'page', link.pathname.slice(1) );
-    ga( 'send', 'pageview' );
-    ga( 'send', 'event', 'Scroll Pageview', link.pathname.slice(1) );
-  });
+    let infScroll = new InfiniteScroll( '.story-list', {
+      path: '.pagination__next',
+      append: 'article.story',
+      history: false,
+      outlayer: msnry,
+      hideNav: '.site-pagination'
+    }).on( 'append', function( event, response, path ) {
+      link.href = response;
+      // console.log( location + link.pathname.slice(1) );
+      ga( 'set', 'page', link.pathname.slice(1) );
+      ga( 'send', 'pageview' );
+      ga( 'send', 'event', 'Scroll Pageview', link.pathname.slice(1) );
+    });
+
+  }
 
   // Animated script header
 
